d92bd9e96c9f791f2ca68915b0ccbe74b2f7ba37
Your Name <leonardo.silva1896@etec.sp.gov.br>
2025-08-01 18:13:17 -0300

    Envio inical

---
+++ projetos_react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Meu app React</h1>
+      <p>ola</p>
+    </div>
+  );
+}
+
+export default App;
