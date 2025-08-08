9d6f5a6f0f741ec576706b1c8d99a557d496560c
Leonardozaia22 <leonardo.silva18962etec.sp.gov.br>
2025-08-08 17:19:34 -0300

    criação de componentes e css

--- projetos_react/src/App.js
+++ projetos_react/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
+import DigaMeuNome from './components/DigaMeuNome';
+
 
 function App() {
   return (
     <div className="App">
       <h1> Meu app React</h1>
-      <p>ola</p>
+      
+      <HelloWorld />
+      <Frase />
+      <DigaMeuNome nome ="Zaia"/>
+      <p>Outro paragrafo</p>
     </div>
   );
 }
